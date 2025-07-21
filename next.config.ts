633fc6239ee5a67be3221c41ad37c225486a687e
Kay Shoor <goodtreeint@gmail.com>
2025-07-21 02:35:38 +0000

    yes I have , ses here

--- next.config.ts
+++ next.config.ts
@@ -18,11 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-};
-
-export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   env: {
     NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
