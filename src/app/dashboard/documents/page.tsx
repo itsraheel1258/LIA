cd7a71fe4fe53441b8d9a55dc49a3108f9b2d556
Kay Shoor <goodtreeint@gmail.com>
2025-08-06 07:16:02 +0000

    my documents tab is simply the same as Your Smart Mailbox

---
+++ src/app/dashboard/documents/page.tsx
@@ -0,0 +1,12 @@
+
+"use client"
+
+import { SmartMailbox } from "@/components/dashboard/smart-mailbox";
+
+export default function DocumentsPage() {
+  return (
+    <div className="p-4 sm:p-6 lg:p-8">
+      <SmartMailbox />
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,14 +2,11 @@
 "use client"
 
 import { DocumentScanner } from "@/components/dashboard/document-scanner";
-import { SmartMailbox } from "@/components/dashboard/smart-mailbox";
-import { Separator } from "@/components/ui/separator";
 
 export default function DashboardPage() {
   return (
     <div className="p-4 sm:p-6 lg:p-8 space-y-8">
       <DocumentScanner />
-      <SmartMailbox />
     </div>
   );
 }
